cc80b8114640aaf8782d10340bdd7667e2d8a184
JJ <juniorj53@gmail.com>
2026-08-28 17:16:23 -0300

    implementada api base cartoes

---
+++ msCartoes/src/main/java/io/github/joseprandj/msCartoes/application/controller/CartoesController.java
@@ -0,0 +1,15 @@
+package io.github.joseprandj.msCartoes.application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cartoes")
+public class CartoesController {
+
+    @GetMapping
+    public String status() {
+        return "OK";
+    }
+}
--- msCartoes/src/test/java/io/github/joseprandj/msCartoes/MsCartoesApplicationTests.java
+++ msCartoes/src/test/java/io/github/joseprandj/msCartoes/MsCartoesApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.joseprandj.mscartoes;
+package io.github.joseprandj.msCartoes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MsCartoesApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
